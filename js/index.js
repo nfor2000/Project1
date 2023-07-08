de59ab56c58fb8f1768436f9c4411b5ca131355a
nfor2000 <nforpresly2@gmail.com>
2023-07-08 05:45:07 +0100

    project update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const newsTitle = document.querySelector(".news_title");
+const newsText = document.querySelector(".news_text");
+
 // JS FOR TIME BOARD
 x = setInterval(function(){
      let date = new Date();
@@ -16,3 +19,58 @@ x = setInterval(function(){
 
 },1000);
 // END
+
+
+var BreakingNews = [
+     {
+          "news-title":"Welcome GPt",
+          "news-image":"../img/icon-complete.svg",
+          "news-text":"Open AI bot"
+     },
+     {
+          "news-title":"",
+          "news-image":"../img/sunk.jpg",
+          "news-text":""
+     },
+     
+] 
+
+//  DYNAMICALLY ADDING BTNS BASED ON BREAKING NEWS AVAILABLE
+
+if (BreakingNews.length > 0)
+{
+     for(let i=0; i < BreakingNews.length; i++)
+     {
+          const slideBtn = document.createElement('button');
+          slideBtn.className = "slider_btn";
+          slideBtn.setAttribute("data-newindex",`${i}`);
+          const parent = document.querySelector('.news_btns');
+          parent.appendChild(slideBtn)
+     };
+
+     const slideBtns = Array.from(document.querySelectorAll(".slider_btn"));
+     slideBtns.forEach( Btn => {
+          Btn.addEventListener("click",function(){
+              let index = Btn.dataset.newindex;
+              newsText.innerHTML = `${BreakingNews[index]["news-text"]}`;
+              newsTitle.innerHTML = `${BreakingNews[index]["news-title"]}`;
+              document.querySelector(".hero").style.background = `linear-gradient(to top,#494944,#49494465) ,url(${BreakingNews[index]["news-image"]}) center center/cover`
+         })
+     })
+
+}
+let index = 0;
+let y = setInterval(function(){
+     
+     if(index >= BreakingNews.length)
+     {
+          index = 0;
+     }
+     document.querySelector(".hero").style.background = `linear-gradient(to top,#494944,#49494465) ,url(${BreakingNews[index]["news-image"]}) center center/cover`
+     newsText.innerHTML = `${BreakingNews[index]["news-text"]}`;
+     newsTitle.innerHTML = `${BreakingNews[index]["news-title"]}`;
+     index = index +1;
+},4000)
+let getParent = (element)=>{
+     return element.parentElement;
+}
