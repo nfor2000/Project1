d7e5302d29aad0049cdfe7a9fe955ab320573232
nfor2000 <nforpresly2@gmail.com>
2023-07-09 20:29:10 +0100

    project update

--- js/index.js
+++ js/index.js
@@ -1,82 +1,50 @@
-const newsTitle = document.querySelector(".news_title");
-const newsText = document.querySelector(".news_text");
+//ARRAY OF DAYS
 
-// JS FOR TIME BOARD
-x = setInterval(function(){
+let Days =["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+//JS FOR DAY DISPLAY
+
+let day = Date().slice(0,3);
+Days.forEach(Day => {
+     if(Day.slice(0,3) == day){
+          document.querySelector(".day").innerHTML = Day.toUpperCase();
+     }
+})
+
+// JS FOR TIME DISPLAY
+
+y = setInterval(function () {
      let date = new Date();
 
-     let hours = date.getHours();
+     let hour = date.getHours();
      let minutes = date.getMinutes();
      let seconds = date.getSeconds();
 
-     document.querySelector(".hours").innerHTML = `${hours} :`;
-     document.querySelector(".minutes").innerHTML = minutes;
-     document.querySelector(".seconds").innerHTML = `: ${seconds}`;
 
-     document.querySelector(".hours_text").innerHTML ="hour";
-     document.querySelector(".minutes_text").innerHTML = (minutes == 1)? "minute":"minutes";
-     document.querySelector(".seconds_text").innerHTML = (seconds == 1)? "second":"seconds";
-
-},1000);
-// END
-
-
-var BreakingNews = [
-     {
-          "news-title":"Welcome GPt",
-          "news-image":"../img/icon-complete.svg",
-          "news-text":"Open AI bot"
-     },
-     {
-          "news-title":"",
-          "news-image":"../img/sunk.jpg",
-          "news-text":""
-     },
      
-] 
+     document.querySelector(".hour").innerHTML = `${hour}:`;
+     document.querySelector(".minutes").innerHTML = `${minutes}:`;
+     document.querySelector(".seconds").innerHTML = `${seconds}`;
 
-//  DYNAMICALLY ADDING BTNS BASED ON BREAKING NEWS AVAILABLE
-
-if (BreakingNews.length > 0)
-{
-     for(let i=0; i < BreakingNews.length; i++)
-     {
-          const slideBtn = document.createElement('button');
-          slideBtn.className = "slider_btn";
-          slideBtn.setAttribute("data-newindex",`${i}`);
-          const parent = document.querySelector('.news_btns');
-          parent.appendChild(slideBtn)
-     };
-     let index = 0;
-     // JS FOR IMAGE SLIDER 
-let y = setInterval(function(){
      
-     if(index >= BreakingNews.length)
-     {
-          index = 0;
-     }
-     document.querySelector(".hero").style.background = `linear-gradient(to top,#494944,#49494465) ,url(${BreakingNews[index]["news-image"]}) center center/cover`
-     newsText.innerHTML = `${BreakingNews[index]["news-text"]}`;
-     newsTitle.innerHTML = `${BreakingNews[index]["news-title"]}`;
-     index = index +1;
-},4000)
-     //CLICK EVENT FOR ROUND BTNS
-     const slideBtns = Array.from(document.querySelectorAll(".slider_btn"));
-     slideBtns.forEach( Btn => {
-          Btn.addEventListener("click",function(){
-              let index = Btn.dataset.newindex;
-              newsText.innerHTML = `${BreakingNews[index]["news-text"]}`;
-              newsTitle.innerHTML = `${BreakingNews[index]["news-title"]}`;
-              document.querySelector(".hero").style.background = `linear-gradient(to top,#494944,#49494465) ,url(${BreakingNews[index]["news-image"]}) center center/cover`
-              setTimeout(()=>{
-                    
-              },4000);
+})
 
-         })
-     })
+// JAVAASCRIPT FOR MENU EVENTS
 
+const navMenu = document.querySelector(".navmenu");
+const navLinks = document.querySelectorAll(".navlink");
+const hamburger = document.querySelector(".hamburger");
+
+// navLinks.forEach(link=>{
+//    link.addEventListener("click",closeMenu)  
+// })
+hamburger.addEventListener("click",OpenMenu)
+
+function OpenMenu(){
+     navMenu.classList.toggle('active');
+     hamburger.classList.toggle('active');
 }
-
-let getParent = (element)=>{
-     return element.parentElement;
+function closeMenu(){
+     navMenu.classList.remove('active');
+     hamburger.classList.remove('active');
 }
