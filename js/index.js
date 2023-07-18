b622d9f4ee266525bda402c49b5f017afd26a440
nfor2000 <nforpresly2@gmail.com>
2023-07-18 08:09:48 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,33 +1,4 @@
-//ARRAY OF DAYS
 
-let Days =["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-
-//JS FOR DAY DISPLAY
-
-
-Days.forEach(Day => {
-     let day = Date().slice(0,3);
-     if(Day.slice(0,3) == day){
-          document.querySelector(".day").innerHTML = Day.toUpperCase();
-     }
-})
-
-// JS FOR TIME DISPLAY
-
-y = setInterval(function () {
-     let date = new Date();
-
-     let hour = date.getHours();
-     let minutes = date.getMinutes();
-     let seconds = date.getSeconds();
-
-     
-     document.querySelector(".hour").innerHTML = `${hour}:`;
-     document.querySelector(".minutes").innerHTML = `${minutes}:`;
-     document.querySelector(".seconds").innerHTML = `${seconds}`;
-
-     
-})
 
 // JAVAASCRIPT FOR MENU EVENTS
 
@@ -47,4 +18,3 @@ function closeMenu(){
      navMenu.classList.remove('active');
      hamburger.classList.remove('active');
 }
-// slider
